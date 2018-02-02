2a280fbd209d23d6c43b4f666d00f5c3f25d20b9
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2018-02-02 13:24:24 +0200

    Add metadata reading

--- fpipy/raw.py
+++ fpipy/raw.py
@@ -3,10 +3,17 @@
 """Reading and interpolating raw CFA data."""
 
 import xarray as xr
+from . import meta
 
+def read_xarray(filepath):
+    """Read a raw CFA datafile to an xarray DataFrame.
+    """
 
-def read_xarray(path):
-    """Read a raw CFA datafile to an xarray DataFrame."""
+    base = os.path.splitext(filepath)[0]
+    datfile = base + '.dat'
+    hdtfile = base + '.hdt'
 
-    cfa = xr.open_rasterio(path)
-    return cfa
+    cfa = xr.open_rasterio(datfile)
+    meta = meta.load_hdt(hdtfile)
+
+    return cfa, meta
