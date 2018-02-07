f69cf246006328a72a15abf7c6e8b6cc8be42cea
Jyri Hämäläinen <jyri.p.hamalainen@gmail.com>
2018-02-07 15:09:48 +0200

    Updated author information

--- fpipy/__init__.py
+++ fpipy/__init__.py
@@ -2,10 +2,8 @@
 
 """Top-level package for Fabry-Perot Imaging in Python."""
 
-__author__ = """Matti A. Eskelinen
-                Jyri Hämäläinen"""
-__email__ = """matti.a.eskelinen@student.jyu.fi
-               jyri.p.hamalainen@gmail.com"""
+__author__ = """fpipy developers"""
+__email__ = 'matti.a.eskelinen@student.jyu.fi'
 __version__ = '0.1.0'
 
 from fpipy.meta import load_hdt, image_meta
