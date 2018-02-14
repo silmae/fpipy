31f20f4c22328ddeaddead87b085044af91b5ed5
Jyri Hämäläinen <jyri.p.hamalainen@gmail.com>
2018-02-14 16:07:28 +0200

    Minor tweaks

--- tests/test_fpipy.py
+++ tests/test_fpipy.py
@@ -10,30 +10,34 @@ import fpipy as fpi
 import xarray as xr
 import numpy as np
 
-@pytest.fixture
-def sample_raw():
-    sample_raw = fpi.read_cfa('../data/house_crop_4b_RAW.dat')
-    return sample_raw
-
-
-def test_raw_envi_file_loading(sample_raw):
-    assert hasattr(sample_raw, 'wavelength')
-
-
-def test_substract_dark_rollover(sample_raw):
-    assert np.all(fpi.raw.substract_dark(sample_raw.cfa) <= sample_raw.cfa)
 
 @pytest.fixture
-def sample_preprocessed_radiance():
-    sample_preprocessed_radiance = xr.open_rasterio('../data/house_crop_4b_RAD.dat')
-    return sample_preprocessed_radiance
+def raw():
+    raw = fpi.read_cfa('data/house_crop_4b_RAW.dat')
+    return raw
+
+
+def test_raw_envi_file_loading(raw):
+    assert hasattr(raw, 'wavelength')
+
+
+def test_substract_dark_rollover(raw):
+    assert np.all(fpi.raw.substract_dark(raw.cfa) <= raw.cfa)
+
 
 @pytest.fixture
-def sample_bilinear_radiance():
-    sample_bilinear_radiance = fpi.raw_to_radiance(sample_raw(), dm_method='bilinear')
-    return sample_bilinear_radiance
+def VTT_radiance():
+    VTT_radiance = xr.open_rasterio('data/house_crop_4b_RAD.dat')
+    return VTT_radiance
 
-def test_matches_bilinear_preprocessed_file(sample_preprocessed_radiance,
-                                            sample_bilinear_radiance):
-    assert pytest.approx(sample_preprocessed_radiance.values,
-                  sample_bilinear_radiance.values)
+
+@pytest.fixture
+def bilinear_radiance(raw):
+    bilinear_radiance = fpi.raw_to_radiance(raw, dm_method='bilinear')
+    return bilinear_radiance
+
+
+def test_matches_bilinear_VTT_file(VTT_radiance,
+                                            bilinear_radiance):
+    assert np.allclose(VTT_radiance.values, bilinear_radiance.values,
+                       rtol=0.275, atol=0.001)
