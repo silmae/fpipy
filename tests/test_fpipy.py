da4a33e8ddc92eaa6b6f63e3c550727723a57721
Jyri Hämäläinen <jyri.p.hamalainen@gmail.com>
2018-02-14 11:05:54 +0200

    Added 3 simple tests, but sample data used in them is too large to push.

--- tests/test_fpipy.py
+++ tests/test_fpipy.py
@@ -6,20 +6,34 @@
 import pytest
 
 
-from fpipy import fpipy
-
+import fpipy as fpi
+import xarray as xr
+import numpy as np
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def sample_raw():
+    sample_raw = fpi.read_cfa('WhiteRef_RAW.dat')
+    return sample_raw
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_raw_envi_file_loading(sample_raw):
+    assert hasattr(sample_raw, 'wavelength')
+
+
+def test_substract_dark_rollover(sample_raw):
+    assert np.any(fpi.raw.substract_dark(sample_raw.cfa) <= 1.0)
+
+@pytest.fixture
+def sample_preprocessed_radiance():
+    sample_preprocessed_radiance = xr.open_rasterio('WhiteRef_RAD.dat')
+    return sample_preprocessed_radiance
+
+@pytest.fixture
+def sample_bilinear_radiance():
+    sample_bilinear_radiance = fpi.raw_to_radiance(sample_raw())
+    return sample_bilinear_radiance
+
+def test_matches_bilinear_preprocessed_file(sample_preprocessed_radiance,
+                                            sample_bilinear_radiance):
+    assert pytest.approx(sample_preprocessed_radiance.values,
+                  sample_bilinear_radiance.values)
