cc40c9835780b6e14963eca6eaa830e789249954
Jyri Hämäläinen <jyri.p.hamalainen@gmail.com>
2018-02-14 14:07:30 +0200

    Edited and texted example.

--- fpipy/raw.py
+++ fpipy/raw.py
@@ -5,11 +5,14 @@
 Example
 -------
 Loading, converting and plotting data can be done as follows::
+    import xarray as xr
+    import fpipy as fpi
+    import matplotlib
 
-    data = fpi.read_cfa('cameraoutput.dat')
+
+    data = fpi.read_cfa('./data/house_crop_4b_RAW.dat')
     radiance = fpi.raw_to_radiance(data)
-    radiance.sel(wavelength=800, method='nearest').plot()
-    radiance.to_netcdf('radiance.nc')
+    radiance.sel(wavelength=600, method='nearest').plot()
 """
 
 import os
