359724d3e1f6bb2cb7fead1206af53c1d6bf4daf
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2018-07-17 10:37:51 +0300

    Add tests for cfa_stack_to_da default parameters

--- tests/test_fpipy.py
+++ tests/test_fpipy.py
@@ -10,6 +10,7 @@ import fpipy as fpi
 from fpipy.data import house_raw, house_radiance
 import xarray as xr
 import numpy as np
+from numpy.testing import assert_array_equal
 
 
 @pytest.fixture
@@ -23,6 +24,7 @@ def rad():
     rad = house_radiance()
     return rad
 
+
 def test_raw_envi_file_loading(raw):
     assert hasattr(raw, 'wavelength')
 
@@ -34,3 +36,15 @@ def test_subtract_dark_rollover(raw):
 def test_radiance_calculation_passes(raw):
     assert type(fpi.raw_to_radiance(raw)) is xr.DataArray
 
+
+@pytest.mark.parametrize('h, exp_y', [(1, [0.5]), (3, [0.5, 1.5, 2.5])])
+@pytest.mark.parametrize('w, exp_x', [(1, [0.5]), (3, [0.5, 1.5, 2.5])])
+@pytest.mark.parametrize('k, exp_i', [(1, [0]), (3, [0, 1, 2])])
+def test_cfa_da_defaults(h, w, k, exp_y, exp_x, exp_i):
+
+    cfa = np.ones((h, w, k))
+    da = fpi.raw.cfa_stack_to_da(cfa, 'RGGB')
+
+    assert_array_equal(da.y, exp_y)
+    assert_array_equal(da.x, exp_x)
+    assert_array_equal(da.index, exp_i)
