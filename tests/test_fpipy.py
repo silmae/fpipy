b89f45400733eab78f43eb9ffd090c71cdaa186c
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2018-07-17 10:45:08 +0300

    Add test for required parameters

--- tests/test_fpipy.py
+++ tests/test_fpipy.py
@@ -37,10 +37,21 @@ def test_radiance_calculation_passes(raw):
     assert type(fpi.raw_to_radiance(raw)) is xr.DataArray
 
 
+def test_cfa_stack_to_da_required_params():
+    with pytest.raises(TypeError, message='') as e:
+        da = fpi.raw.cfa_stack_to_da()
+    assert 'missing 2 required positional arguments' in str(e.value)
+
+    with pytest.raises(TypeError) as e:
+        cfa = np.ones((2,2,2))
+        da = fpi.raw.cfa_stack_to_da(cfa)
+    assert 'missing 1 required positional argument' in str(e.value)
+
+
 @pytest.mark.parametrize('h, exp_y', [(1, [0.5]), (3, [0.5, 1.5, 2.5])])
 @pytest.mark.parametrize('w, exp_x', [(1, [0.5]), (3, [0.5, 1.5, 2.5])])
 @pytest.mark.parametrize('k, exp_i', [(1, [0]), (3, [0, 1, 2])])
-def test_cfa_da_defaults(h, w, k, exp_y, exp_x, exp_i):
+def test_cfa_stack_to_da_defaults(h, w, k, exp_y, exp_x, exp_i):
 
     cfa = np.ones((h, w, k))
     da = fpi.raw.cfa_stack_to_da(cfa, 'RGGB')
