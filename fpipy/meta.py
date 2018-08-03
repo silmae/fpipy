6490bbd6745798bd9231e9886a8871c24b796345
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2018-08-03 17:39:12 +0300

    Rework metadata parsing

--- fpipy/meta.py
+++ fpipy/meta.py
@@ -3,13 +3,38 @@
 """Metadata parsing."""
 
 import numpy as np
+import xarray as xr
 
 
-def image_meta(meta, idx):
+def parse_meta_to_ds(meta):
+    """Parse FPI metadata from a configparser to a xarray.Dataset."""
+
+    header = meta['Header']
+    attrs = {
+        'FPI temperature': header.getfloat('FPI Temperature'),
+        'dark layer included':
+            header.getboolean('Dark Layer included'),
+        'description': header['Description'].strip('"')
+        }
+
+    layers = meta.sections()
+    layers.remove('Header')
+    if attrs['dark layer included']:
+        layers.remove('Image0')
+
+    ds = xr.concat(
+            [parse_image_meta(meta, layer) for layer in layers],
+            dim='index'
+            )
+
+    ds = ds.assign_attrs(attrs)
+    return ds
+
+
+def parse_image_meta(meta, layer):
     """Parse metadata for a given image (layer) in the FPI data."""
 
-    layer = 'Image{}'.format(idx)
-    im_meta = dict()
+    im_meta = xr.Dataset()
     im_meta['npeaks'] = meta.getint(layer, 'npeaks')
     im_meta['width'] = meta.getint(layer, 'width')
     im_meta['height'] = meta.getint(layer, 'height')
@@ -17,28 +42,42 @@ def image_meta(meta, idx):
     im_meta['exposure time'] = meta.getfloat(layer, 'exposure time (ms)')
     im_meta['bayer pattern'] = meta.getint(layer, 'bayer pattern')
     im_meta['index'] = meta.getint(layer, 'index')
-    im_meta['wavelengths'] = parsevec(meta.get(layer, 'wavelengths'))
-    im_meta['fwhms'] = parsevec(meta.get(layer, 'fwhms'))
-    im_meta['setpoints'] = parsevec(meta.get(layer, 'setpoints'))
-    im_meta['sinvs'] = parsesinvs(meta.get(layer, 'sinvs'))
+    im_meta['wavelength'] = parse_peakmeta(meta.get(layer, 'wavelengths'))
+    im_meta['fwhm'] = parse_peakmeta(meta.get(layer, 'fwhms'))
+    im_meta['setpoint'] = parse_setpoints(meta.get(layer, 'setpoints'))
+    im_meta['sinv value'] = parse_sinvs(meta.get(layer, 'sinvs'))
+
     return im_meta
 
 
-def metalist(meta, quantity):
-    """Creates a list from a metadata quantity across images."""
+def parse_peakmeta(s):
+    return xr.DataArray(
+            parsevec(s),
+            dims=('peak'),
+            coords={'peak': [1, 2, 3]}
+            )
 
-    return [image_meta(meta, layer)[quantity]
-            for layer in range(0, int(meta['Header']['number of layers']))]
+
+def parse_setpoints(s):
+    return xr.DataArray(
+            parsevec(s),
+            dims=('setpoint_index'),
+            coords={'setpoint_index': ['SP1', 'SP2', 'SP3']}
+            )
+
+
+def parse_sinvs(s):
+    """Parse an array of floats from a string."""
+    vector = parsevec(s)
+    return xr.DataArray(
+            [vector[0:3], vector[3:6], vector[6:]],
+            dims=('peak', 'colour'),
+            coords={
+                'peak': [1, 2, 3],
+                'colour': ['R', 'G', 'B']}
+            )
 
 
 def parsevec(s):
     """Parse a vector of floats from a string."""
-
     return np.fromstring(s.strip('"'), dtype='float', sep=' ')
-
-
-def parsesinvs(s):
-    """Parse an array of floats from a string."""
-
-    vector = parsevec(s)
-    return np.array([vector[0:3], vector[3:6], vector[6:]])
