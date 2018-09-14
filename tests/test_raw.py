7c1545e25bf27ad9c945e4fec51154056239ab67
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2018-09-14 19:33:57 +0300

    Add tests for format conventions

--- tests/test_raw.py
+++ tests/test_raw.py
@@ -64,6 +64,7 @@ def raw():
 
     return raw
 
+
 def test_ENVI_raw_format(raw, raw_ENVI):
     assert type(raw_ENVI) is type(raw)
 
@@ -78,6 +79,8 @@ def test_ENVI_raw_format(raw, raw_ENVI):
 def test_raw_format(raw):
     assert type(raw) is xr.Dataset
 
+    # These must be found from each raw dataset for radiance calculations to be
+    # possible
     dims = [
         c.peak_coord,
         c.colour_coord,
@@ -89,24 +92,40 @@ def test_raw_format(raw):
     variables = [
         c.number_of_peaks,
         c.camera_exposure,
-        c.cfa_pattern_attribute,
+        c.cfa_pattern_data,
         c.wavelength_data,
-#        c.fwhm_data,
         c.sinv_data,
-#        c.dark_reference_data,
         c.cfa_data
         ]
     for v in variables:
         assert v in raw.variables
 
 
+def test_raw_dark_inclusion(raw):
+    # If a dataset contains data and a dark current reference, it must indicate
+    # whether it has been subtracted already
+
+    if (c.cfa_data in raw.variables and
+       c.dark_reference_data in raw.variables):
+        assert c.dc_included_attr in raw[c.cfa_data].attrs
+        assert type(raw[c.cfa_data].attrs[c.dc_included_attr]) is bool
+
+
 def test_raw_to_radiance_format(raw):
     rad = fpr.raw_to_radiance(raw)
     assert type(rad) is xr.Dataset
 
+    # These should exist in radiances computed from CFA data
     dims = c.radiance_dims
     for d in dims:
         assert d in rad.dims
+    variables = [
+        c.image_index,
+        c.peak_coord,
+        c.wavelength_data,
+        ]
+    for v in variables:
+        assert v in rad.variables
 
 
 def test_subtract_dark(raw):
