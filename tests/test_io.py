4e4928918177cd8508302d135bba66c1178b59bb
Leevi Annala <leeviannala@gmail.com>
2019-10-22 15:59:55 +0300

    Changed the filepaths

--- tests/test_io.py
+++ tests/test_io.py
@@ -2,9 +2,9 @@ from fpipy.io import read_calibration
 import xarray as xr
 
 def test_read_calibration():
-    cube_expected = xr.open_dataset("C:/MyTemp/fpipy/fpipy/data/read_calibration_expected_result.nc")
-    cube = read_calibration("C:/MyTemp/fpipy/fpipy/data/test_calib.txt")
-    cube2 = read_calibration("C:/MyTemp/fpipy/fpipy/data/house_calib_seq.txt")
+    cube_expected = xr.open_dataset("./fpipy/data/read_calibration_expected_result.nc")
+    cube = read_calibration("./fpipy/data/test_calib.txt")
+    cube2 = read_calibration("./fpipy/data/house_calib_seq.txt")
     assert cube == cube_expected
     assert cube2 == cube_expected
     
